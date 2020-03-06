1c4e2c6999b57d58ce5bd8abbbedcc462ff56b7a
benjamincarney <carneyb@umich.edu>
2020-03-06 10:59:56 -0500

    Okay now I actually fixed the error

--- Accev/Modules/PrimaryMap/HomeController.swift
+++ Accev/Modules/PrimaryMap/HomeController.swift
@@ -59,8 +59,9 @@ class HomeController: RoutedViewController {
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         marker.title = "Your mom's"
         marker.snippet = "house"
-        // swiftlint:disable force_cast
+        // swiftlint:disable all
         let customPin = UIImage(named: "pin-9-32")!.withRenderingMode(.alwaysTemplate)
+        // swiftlint:enable all
         marker.iconView = UIImageView(image: customPin)
         marker.map = mapView
     }
