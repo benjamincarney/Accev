b5b01d395d3e816f971319586e700f36455f45ad
benjamincarney <carneyb@umich.edu>
2020-03-14 12:23:14 -0400

    More progress

--- Accev/Modules/PinDetails/PinDetailsController.swift
+++ Accev/Modules/PinDetails/PinDetailsController.swift
@@ -40,6 +40,7 @@ class PinDetailsController: UIViewController {
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "x24blue").withRenderingMode(.alwaysOriginal),
                                                            style: .plain, target: self,
                                                            action: #selector(handleDismiss))
+        
 
         self.view.addSubview(upvotesLabel)
         self.view.addSubview(downvotesLabel)
@@ -54,10 +55,26 @@ class PinDetailsController: UIViewController {
         hearingLabel.translatesAutoresizingMaskIntoConstraints = false
 
         upvotesLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
-        upvotesLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 110).isActive = true
+        upvotesLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 115).isActive = true
+        
+        let imageUp = UIImage(named: "thumbup24.png")
+        let imageViewUp = UIImageView(image: imageUp)
+        imageViewUp.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        view.addSubview(imageViewUp)
+        imageViewUp.translatesAutoresizingMaskIntoConstraints = false
+        imageViewUp.leftAnchor.constraint(equalTo: upvotesLabel.rightAnchor, constant: 10).isActive = true
+        imageViewUp.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 110).isActive = true
 
-        downvotesLabel.leftAnchor.constraint(equalTo: upvotesLabel.rightAnchor, constant: 20).isActive = true
-        downvotesLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 110).isActive = true
+        downvotesLabel.leftAnchor.constraint(equalTo: imageViewUp.rightAnchor, constant: 20).isActive = true
+        downvotesLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 114).isActive = true
+        
+        let imageDown = UIImage(named: "thumbdown24.png")
+        let imageViewDown = UIImageView(image: imageDown)
+        imageViewDown.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        view.addSubview(imageViewDown)
+        imageViewDown.translatesAutoresizingMaskIntoConstraints = false
+        imageViewDown.leftAnchor.constraint(equalTo: downvotesLabel.rightAnchor, constant: 10).isActive = true
+        imageViewDown.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 110).isActive = true
         
         wheelchairLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
         wheelchairLabel.topAnchor.constraint(equalTo: upvotesLabel.bottomAnchor, constant: 40).isActive = true
@@ -172,10 +189,18 @@ class PinDetailsController: UIViewController {
         label.font = R.font.latoRegular(size: 25)
         return label
     }()
+    
+    lazy var ratePinLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        label.text = "Rate this pin!"
+        label.textColor = Colors.behindGradient
+        label.font = R.font.latoRegular(size: 25)
+        return label
+    }()
 
     lazy var upvotesLabel: UILabel = {
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        label.text = "\(Int(self.upvotes!)) upvotes"
+        label.text = "\(Int(self.upvotes!))"
         label.textColor = UIColor(red: 70.0 / 255.0, green: 151.0 / 255.0, blue: 47.0 / 255.0, alpha: 1.0)
         label.font = R.font.latoRegular(size: 17)
         return label
@@ -183,7 +208,7 @@ class PinDetailsController: UIViewController {
 
     lazy var downvotesLabel: UILabel = {
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        label.text = "\(Int(self.downvotes!)) downvotes"
+        label.text = "\(Int(self.downvotes!))"
         label.textColor = UIColor(red: 207.0 / 255.0, green: 15.0 / 255.0, blue: 25.0 / 255.0, alpha: 1.0)
         label.font = R.font.latoRegular(size: 17)
         return label
